bafb0ab84d5c211ac6cd7c0271b973696aefa649
Mathilde YERNAUX <myernaux@e2r13p4.42.fr>
2017-05-24 12:07:05 +0200

    unicode

--- srcs/ft_putwchar.c
+++ srcs/ft_putwchar.c
@@ -6,33 +6,81 @@
 /*   By: myernaux <myernaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 15:21:17 by myernaux          #+#    #+#             */
-/*   Updated: 2017/05/24 10:40:57 by myernaux         ###   ########.fr       */
+/*   Updated: 2017/05/24 11:57:24 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int    ft_putwchar(wchar_t wc)
+void    init_wchar(unsigned char *f, wchar_t c, int mode)
 {
-    if (wc <= 127)
-        ft_putchar(wc);
-    else if (wc <= 2047)
+    if (mode == 2)
     {
-        ft_putchar((wc >> 6) + 0xC0);
-        ft_putchar((wc & 0x3F) + 0x80);
+        f[0] = (((c & 0x07c0) >> 6) + 0xC0);
+        f[1] = (((c & 0x003F)) + 0x80);
     }
-    else if (wc <= 65535)
+    if (mode == 3)
     {
-        ft_putchar((wc >> 12) + 0xE0);
-        ft_putchar(((wc >> 6) & 0x3F) + 0x80);
-        ft_putchar((wc & 0x3F) + 0x80);
+        f[0] = (((c & 0xF000) >> 12) + 0xE0);
+        f[1] = (((c & 0x0FC0) >> 6) + 0x80);
+        f[2] = (((c & 0x003F)) + 0x80);
     }
-    else if (wc <= 1114111)
+    if (mode == 4)
     {
-        ft_putchar((wc >> 18) + 0xF0);
-        ft_putchar(((wc >> 12) & 0x3F) + 0x80);
-        ft_putchar(((wc >> 6) & 0x3F) + 0x80);
-        ft_putchar((wc & 0x3F) + 0x80);
+        f[0] = (((c & 0x1C0000) >> 18) + 0xF0);
+        f[1] = (((c & 0x03F000) >> 12) + 0x80);
+        f[2] = (((c & 0x0FC0) >> 6) + 0x80);
+        f[3] = (((c & 0x003F)) + 0x80);
     }
+}
+
+void    ft_putnchar(unsigned char *f, int size)
+{
+    int i;
+
+    i = 0;
+    while (i != size)
+    {
+        write(1, &f[i], 1);
+        i++;
+    }
+}
+
+char    ft_print_w(unsigned char *f, unsigned int e, wchar_t c)
+{
+    if (e < 0x07FF)
+    {
+        init_wchar(f, c, 2);
+        ft_putnchar(f, 2);
+        return (2);
+    }
+    else if (e < 0xFFFF)
+    {
+        init_wchar(f, c, 3);
+        ft_putnchar(f, 3);
+        return (3);
+    }
+    else
+    {
+        init_wchar(f, c, 4);
+        ft_putnchar(f, 4);
+        return (4);
+    }
+}
+
+int     ft_putwchar(wchar_t c)
+{
+    unsigned char    f[4];
+    unsigned int    e;
+
+    e = (unsigned int)c;
+    ft_bzero(f, 4);
+    if (c >= 0 && c <= 255)
+    {
+        ft_putchar(c);
+        return (1);
+    }
+    else if (e > 255 && e < 0x10FFFF)
+        return (ft_print_w(f, e, c));
     return (1);
 }
--- srcs/ft_putwstr.c
+++ srcs/ft_putwstr.c
@@ -6,7 +6,7 @@
 /*   By: myernaux <myernaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 15:21:14 by myernaux          #+#    #+#             */
-/*   Updated: 2017/05/24 10:44:05 by myernaux         ###   ########.fr       */
+/*   Updated: 2017/05/24 11:59:49 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 int     ft_putwstr(wchar_t *wstr)
 {
     int i;
-    int j;
 
-    j = -1;
     i = 0;
-    while (wstr[++j])
+    while (*wstr)
     {
-        ft_putwchar(wstr[j]);
+        i += ft_putwchar(*wstr);
         wstr++;
-        i++;
     }
     return (i);
 }
