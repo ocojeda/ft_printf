2fd7a7cb5d6c5e539bbfc92d94baa944f463d98b
Mathilde YERNAUX <myernaux@e2r13p4.42.fr>
2017-06-07 14:24:41 +0200

    Norme OK

--- srcs/ft_putwstr2.c
+++ srcs/ft_putwstr2.c
@@ -6,13 +6,13 @@
 /*   By: myernaux <myernaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 08:58:01 by myernaux          #+#    #+#             */
-/*   Updated: 2017/06/06 08:59:20 by myernaux         ###   ########.fr       */
+/*   Updated: 2017/06/07 14:24:26 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int 	putwstr6(int everything, wchar_t *str_temp)
+int		putwstr6(int everything, wchar_t *str_temp)
 {
 	everything += ft_wstrlen(str_temp);
 	ft_wputstr(str_temp);
@@ -30,7 +30,7 @@ void	putwstr5(t_type *temp, wchar_t *wstr, wchar_t *str_temp, int i)
 	free(str_temp);
 }
 
-int	putwstr4(t_type *temp, wchar_t *str_temp, int everything, int total)
+int		putwstr4(t_type *temp, wchar_t *str_temp, int everything, int total)
 {
 	if (total < temp->pres_right)
 		total -= temp->pres_right;
@@ -38,7 +38,7 @@ int	putwstr4(t_type *temp, wchar_t *str_temp, int everything, int total)
 		total = ft_wstrlen(str_temp);
 	if (temp->pres_right > total)
 		everything += total;
-	else 
+	else
 		everything += temp->pres_right;
 	return (everything);
 }
