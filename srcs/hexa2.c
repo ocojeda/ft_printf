1b8e0c565c205a1c6117c104345e49c177d58b16
Mathilde YERNAUX <myernaux@e2r13p4.42.fr>
2017-06-07 14:23:27 +0200

    Norme OK

--- srcs/hexa2.c
+++ srcs/hexa2.c
@@ -6,13 +6,13 @@
 /*   By: myernaux <myernaux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 08:46:59 by myernaux          #+#    #+#             */
-/*   Updated: 2017/06/07 12:36:11 by myernaux         ###   ########.fr       */
+/*   Updated: 2017/06/07 14:23:09 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static	int	switch_hexam(int x)
+static int	switch_hexam(int x)
 {
 	if (0 <= x && x <= 9)
 		return (48 + x);
@@ -52,7 +52,7 @@ char		*ft_hexa_itoa_capital(unsigned long long n)
 	return (str);
 }
 
-int		ft_putx(t_type *temp, int all)
+int			ft_putx(t_type *temp, int all)
 {
 	if (temp->type == HEXAM)
 		ft_putstr("0X");
@@ -61,7 +61,7 @@ int		ft_putx(t_type *temp, int all)
 	return (all += 2);
 }
 
-int	set_presschar2(t_type *temp, int i, int total, int all)
+int			set_presschar2(t_type *temp, int i, int total, int all)
 {
 	while (i > 0)
 	{
@@ -87,21 +87,21 @@ int	set_presschar2(t_type *temp, int i, int total, int all)
 	return (all);
 }
 
-int	set_presschar(t_type *temp, int total)
+int			set_presschar(t_type *temp, int total)
 {
-	int i;
-	int all;
+	int		i;
+	int		all;
 
 	all = 0;
 	i = 0;
-	if (temp->pres_left < total && temp->pres_right < total && 
+	if (temp->pres_left < total && temp->pres_right < total &&
 		temp->hash_tag == 2)
 		all = ft_putx(temp, all);
 	if (temp->pres_left > total || temp->pres_right > total)
 	{
 		if (temp->pres_right < total)
 			temp->pres_right = total;
-		if (temp->type == HEXA && temp->nopoint && temp->hash_tag && 
+		if (temp->type == HEXA && temp->nopoint && temp->hash_tag &&
 		temp->pres_right > total + 2 && !temp->pres_left)
 			temp->pres_right -= 2;
 		if (temp->pres_left > temp->pres_right)
